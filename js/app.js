0db93ce1b81a41a39da9031718808bd4317342e1
codetective <iaminiodu@gmail.com>
2020-08-12 14:36:48 -0700

    added retrictions for non-admin users

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ let loader = document.querySelector('.loader')
 const tsbx = document.querySelector('.toastbox')
 const spinner = document.querySelector('#spinner-wrapper')
 
-const url = 'https://shoprecord.herokuapp.com'
+// const url = 'https://shoprecord.herokuapp.com'
+const url = 'http://localhost:3000'
 const showToast = (msg, color) => {
   const toastbx = document.createElement('div')
   toastbx.classList.add('toastbx')
@@ -64,16 +65,17 @@ const login = async data => {
     if (result.error) {
       showToast(result.error, 'red')
       return
-    } else if (result.name === 'admin') {
+    } else {
       showToast(result.msg, 'green')
-      localStorage.setItem('shopperutilscode', result.name)
-      indicator.innerText = `(${status})`
+      await localStorage.setItem('shopperutilscode', result.name)
+
       main.hidden = false
       loginSection.hidden = true
       loginSection.remove()
       app()
-    } else {
-      showToast('not allowed', 'red')
+      indicator.innerText = `(${localStorage.getItem('shopperutilscode')})`
+      status = localStorage.getItem('shopperutilscode')
+
     }
   } catch (error) {
     loader.style.display = 'none'
@@ -82,7 +84,12 @@ const login = async data => {
 }
 
 let app = () => {
-  async function FetchStock () {
+  if (localStorage.getItem('shopperutilscode') !== 'admin') {
+    let t = document.querySelector('#accordion-wrapper')
+    t.remove()
+  }
+
+  async function FetchStock() {
     try {
       const fetchResult = fetch(
         new Request(url, { method: 'GET', cache: 'reload' })
@@ -94,7 +101,6 @@ let app = () => {
         spinner.style.display = 'none'
         showToast('stocks updated', 'green')
         mapStocks(jsonData.stocks)
-        mapOptions(jsonData.stocks)
       } else {
         showToast('error encountered. Please retry', 'red')
       }
@@ -108,73 +114,92 @@ let app = () => {
   FetchStock()
 
   const DeleteStock = async stockname => {
-    spinner.style.display = 'flex'
-    cartArray = ['']
-    cartItems.innerHTML = ''
-    mapCartSum()
-    regSaleBtn.classList.remove('disabled')
+    if (status !== 'admin') {
+      return;
+    } else {
+      spinner.style.display = 'flex'
+      cartArray = ['']
+      cartItems.innerHTML = ''
+      mapCartSum()
+      regSaleBtn.classList.remove('disabled')
 
-    try {
-      const response = await fetch(url, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: stockname
-      })
-      const result = await response.json()
-      FetchStock()
-    } catch (error) {
-      showToast(error)
-      spinner.style.display = 'none'
+      try {
+        const response = await fetch(url, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: stockname
+        })
+        const result = await response.json()
+        FetchStock()
+      } catch (error) {
+        showToast(error)
+        spinner.style.display = 'none'
+      }
     }
   }
 
-  const AddNewStock = async data => {
-    spinner.style.display = 'flex'
 
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: data
-      })
-      addStockForm.reset()
-      const result = await response.json()
-      if (result.msg) {
+  const AddNewStock = async data => {
+    if (status !== 'admin') {
+      return;
+    } else {
+      spinner.style.display = 'flex'
+
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: data
+        })
+        addStockForm.reset()
+        const result = await response.json()
+        if (result.msg) {
+          spinner.style.display = 'none'
+          showToast(result.msg, 'red')
+        } else {
+          FetchStock()
+        }
+      } catch (error) {
         spinner.style.display = 'none'
-        showToast(result.msg, 'red')
-      } else {
-        FetchStock()
+        showToast(error, 'red')
       }
-    } catch (error) {
-      spinner.style.display = 'none'
-      showToast(error, 'red')
     }
   }
   const UpdateStock = async data => {
-    spinner.style.display = 'flex'
+    if (status !== 'admin') {
+      return;
+    } else {
+      if (status !== 'admin') {
+        return;
+      } else {
+        spinner.style.display = 'flex'
 
-    try {
-      const response = await fetch(url, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: data
-      })
-      stockUpdateForm.reset()
-      const result = await response.json()
-      FetchStock()
-    } catch (error) {
-      spinner.style.display = 'none'
+        try {
+          const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: data
+          })
+          stockUpdateForm.reset()
+          const result = await response.json()
+          FetchStock()
+        } catch (error) {
+          spinner.style.display = 'none'
 
-      showToast(error, 'red')
+          showToast(error, 'red')
+        }
+      }
     }
   }
+
   const RegisterSale = async sale => {
+  
     spinner.style.display = 'flex'
 
     try {
@@ -211,6 +236,8 @@ let app = () => {
           </div>`
 
       stockbox.appendChild(div)
+      mapOptions(stocks)
+
     })
   }
 
@@ -229,7 +256,7 @@ let app = () => {
 
   const labels = document.querySelectorAll('.accordion-item__label')
   const tabs = document.querySelectorAll('.accordion-tab')
-  function toggleShow () {
+  function toggleShow() {
     const target = this
     const item = target.classList.contains('accordion-tab')
       ? target
@@ -268,17 +295,58 @@ let app = () => {
   let cartSum = document.querySelector('#cart-sum')
   let cartItemsList = document.getElementsByClassName('cart-item')
   let regSaleBtn = document.querySelector('#cart-submit')
+   
   let addStockForm = document.querySelector('#addStockForm')
   let stockUpdateForm = document.querySelector('#stockUpdateForm')
   let stockUpdateName = document.querySelector('#stockUpdateName')
+//do cart events
+
+cartItems.addEventListener('click', e => {
+  if (e.target.classList.contains('cart-item')) {
+    e.target.parentNode.removeChild(e.target)
+    let curr = cartArray.indexOf(e.target.textContent)
+    cartArray.splice(curr, 1)
+    mapCartSum()
+  }
+})
+
+regSaleBtn.addEventListener('click', () => { 
+  regSaleBtn.classList.add('disabled')
+  const sales = []
+  for (let i = 0; i < cartItemsList.length; i++) {
+    const element = cartItemsList[i]
+    let item = {
+      name: element.dataset.cartname,
+      qty: parseInt(element.children[1].value)
+    }
+
+    if (item.qty < 1) {
+      alert('Values cannot be 0')
+      setTimeout(() => {
+        regSaleBtn.classList.remove('disabled')
+      }, 1500)
+      return
+    } else {
+      spinner.style.display = 'block'
+
+      sales.push(item)
+    }
+  }
+
+  RegisterSale(JSON.stringify(sales))
+})
 
   const mapOptions = stocks => {
+    if (status !== 'admin') {
+      return;
+    } else {
     stockUpdateName.innerHTML = ''
     stocks.forEach(stock => {
       let opt = document.createElement('option')
       opt.innerText = stock.name
       stockUpdateName.appendChild(opt)
     })
+  }
   }
   const mapCartSum = () => {
     cartSum.innerText = cartItemsList.length
@@ -303,6 +371,10 @@ let app = () => {
       DeleteStock(JSON.stringify({ name: e.target.dataset.stockname }))
     }
   })
+
+    if (status !== 'admin') {
+    return;
+  } else {
   addStockForm.addEventListener('submit', e => {
     e.preventDefault()
     const formdata = new FormData(addStockForm)
@@ -328,46 +400,11 @@ let app = () => {
 
     UpdateStock(data)
   })
-  //do cart events
+}
 
-  cartItems.addEventListener('click', e => {
-    if (e.target.classList.contains('cart-item')) {
-      e.target.parentNode.removeChild(e.target)
-      let curr = cartArray.indexOf(e.target.textContent)
-      cartArray.splice(curr, 1)
-      mapCartSum()
-    }
-  })
-
-  regSaleBtn.addEventListener('click', () => {
-    regSaleBtn.classList.add('disabled')
-    const sales = []
-    for (let i = 0; i < cartItemsList.length; i++) {
-      const element = cartItemsList[i]
-      let item = {
-        name: element.dataset.cartname,
-        qty: parseInt(element.children[1].value)
-      }
-
-      if (item.qty < 1) {
-        alert('Values cannot be 0')
-        setTimeout(() => {
-          regSaleBtn.classList.remove('disabled')
-        }, 1500)
-        return
-      } else {
-        spinner.style.display = 'block'
-
-        sales.push(item)
-      }
-    }
-
-    RegisterSale(JSON.stringify(sales))
-  })
-
-  let logout = document.querySelector('#logout')
-  logout.onclick = () => {
-    localStorage.removeItem('shopperutilscode')
-    location.reload();
-  }
+}
+let logout = document.querySelector('#logout')
+logout.onclick = () => {
+  localStorage.removeItem('shopperutilscode')
+  location.reload();
 }
